e596b418c95555f636f5607fa7484d36f7e8b374
Victor Lopes <Victor Lopes>
2019-08-03 14:11:35 -0400

    View orgao adicionada

---
+++ front/src/views/Orgao.vue
@@ -0,0 +1,32 @@
+<template>
+    <vContainer>
+        <VLayout wrap>
+            <VFlex>
+                <VCard style="min-height:10em;" >
+                    <vContainer style="" >
+                        <VCard style="text-align:center; font-weight:bold;width:40%;float:left;margin-right:1em;">
+                            <VIcon style="font-size:3em; color:purple;">mdi-school</VIcon>
+                            <VCardText>Educação</VCardText>
+                        </VCard>
+                        <vContainer style="margin-left:5%;">
+                            <h4>Nº de licitações:</h4>
+                            <h4>Avaliação: 5.0</h4>
+                        </vContainer>
+                        
+                    </vContainer>
+                    
+                </VCard>
+            </VFlex>
+        </VLayout>
+    </vContainer>
+</template>
+
+<script>
+export default {
+    name:'orgao'
+}
+</script>
+
+<style>
+
+</style>
