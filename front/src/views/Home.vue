aead8f5b68b28e0fb94881b11e4d1e9333e71bcd
Fabio Ferreira <fabiomataferreira@gmail.com>
2019-08-03 21:09:07 -0400

    Ajusta favicon e titulo

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,21 +1,15 @@
 <template>
   <v-container class="home">
     <v-layout wrap v-if="!loading">
-          <v-flex
-              
-              style="padding: 5px" 
-              v-for="(categoria, key) in categorias"
-              :key="key"
-              xs6
-            >
-              <v-card @click="irParaDetalhes(categoria)" class="card" height="150px" link>
-                <v-icon color="purple" :size="80">{{categoria.icone}}</v-icon>
-                <span>{{categoria.titulo}}</span>
-              </v-card>
-            </v-flex>
+      <v-flex style="padding: 5px" v-for="(categoria, key) in categorias" :key="key" xs6>
+        <v-card @click="irParaDetalhes(categoria)" class="card" height="150px" link>
+          <v-icon color="purple" :size="80">{{categoria.icone}}</v-icon>
+          <span>{{categoria.titulo}}</span>
+        </v-card>
+      </v-flex>
     </v-layout>
     <VLayout v-else>
-      <v-flex >
+      <v-flex>
         <Loading />
       </v-flex>
     </VLayout>
@@ -23,33 +17,35 @@
 </template>
 
 <script>
-import axios from 'axios';
-import Loading from '@/components/Loading.vue';
+import axios from "axios";
+import Loading from "@/components/Loading.vue";
 export default {
-  name: 'home',
+  name: "home",
   data() {
     return {
       loading: true,
-      categorias:[]
-    }
+      categorias: []
+    };
   },
   created() {
-    axios.get('http://localhost:86/api/categorias').then(res => {
-      this.categorias = res.data
-      this.loading = false
-    })
+    axios.get("http://localhost:86/api/categorias").then(res => {
+      this.categorias = res.data;
+      this.loading = false;
+    });
   },
   methods: {
     irParaDetalhes(categoria) {
-      const {link, icone} = categoria
-      let chave = Object.keys(this.categorias).find(key => this.categorias[key] == categoria)
+      const { link, icone } = categoria;
+      let chave = Object.keys(this.categorias).find(
+        key => this.categorias[key] == categoria
+      );
       this.$router.push({
-        path:`/orgao/${chave}`,
-        params: {link, icone}
-        })
+        path: `/orgao/${chave}`,
+        params: { link, icone }
+      });
     }
   },
-  components:{
+  components: {
     Loading
   }
 };
@@ -58,7 +54,7 @@ export default {
 <style lang="scss" scoped>
 .home {
   margin: 1rem 0;
-  .card{
+  .card {
     display: flex;
     flex-direction: column;
     justify-content: center;
