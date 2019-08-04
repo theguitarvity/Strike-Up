af1590f4d66c891d027c73a3481f7f13ef4e9b9b
Victor Lopes <Victor Lopes>
2019-08-03 20:49:02 -0400

    [NEW] Adicionando loading page e modificando sua cor

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,54 +1,45 @@
 <template>
   <v-container class="home">
-    <v-layout wrap>
-        <v-flex
-          style="padding: 5px" 
-          v-for="(categoria, key) in categorias"
-          :key="key"
-          xs6
-        >
-          <v-card @click="irParaDetalhes(categoria)" class="card" height="150px" link>
-            <v-icon color="purple" :size="80">{{categoria.icone}}</v-icon>
-            <span>{{categoria.titulo}}</span>
-          </v-card>
-        </v-flex>
+    <v-layout wrap v-if="!loading">
+        
+          <v-flex
+              
+              style="padding: 5px" 
+              v-for="(categoria, key) in categorias"
+              :key="key"
+              xs6
+            >
+              <v-card @click="irParaDetalhes(categoria)" class="card" height="150px" link>
+                <v-icon color="purple" :size="80">{{categoria.icone}}</v-icon>
+                <span>{{categoria.titulo}}</span>
+              </v-card>
+            </v-flex>
+            
+       
     </v-layout>
+    <VLayout v-else>
+      <v-flex >
+        <Loading />
+      </v-flex>
+    </VLayout>
   </v-container>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
+import Loading from '@/components/Loading.vue';
 export default {
   name: 'home',
   data() {
     return {
-      categorias: [
-        {
-          id: 1,
-          nome: 'Educação',
-          icone: 'mdi-school'
-        },
-        {
-          id: 2,
-          nome: 'Saúde',
-          icone: 'mdi-heart-outline'
-        },
-        {
-          id: 3,
-          nome: 'Segurança',
-          icone: 'mdi-security'
-        },
-        {
-          id: 4,
-          nome: 'Infraestrutura',
-          icone: 'mdi-office-building'
-        },
-      ]
+      loading: true,
+      categorias:[]
     }
   },
   created() {
     axios.get('http://localhost:86/api/categorias').then(res => {
       this.categorias = res.data
+      this.loading = false
     })
   },
   methods: {
@@ -60,6 +51,9 @@ export default {
         params: {link, icone}
         })
     }
+  },
+  components:{
+    Loading
   }
 };
 </script>
